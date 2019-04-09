e0d9202d0739e690eb70fe12e69bc1e611e5e31d
David Bailey <46795205+twsdbailey@users.noreply.github.com>
2019-04-08 19:20:38 -0500

    Add files via upload

--- ArduinoSerialParser.ino
+++ ArduinoSerialParser.ino
@@ -1,44 +1,48 @@
 /* SMM Serial data parser and COM protocol for Arduino I/O pin control, v1
-   modified 3/30/2019 from source code written by Robin2 @ http://forum.arduino.cc/index.php?topic=396450
-   by D. Bailey, Science Museum of MN
+   modified and adapted from example code written by Robin2 @ http://forum.arduino.cc/index.php?topic=396450
+   4/8/2019
+   by D. Bailey, Science Museum of Minnesota
 
-   To be used with Arduino UNO, or Pro-Mini, and SMM stele/electron-wrapper
+   Tested with Arduino UNO R3 @ 115200 baud, Adafruit Metro Mini @ 115200 baud, and Inland Pro-Mini @ 9600 baud
 
 ********Incoming serial data***************
 
   Valid string data received by the Arduino is parsed into four parts:
-  pin: number, state: of pin (1 or 0), PWM value (set range0-255), retrieve analog value (returned range 0-1023)
-
+  pin number, state of pin (1=high, 0=low), pwm value (range 0-255), current analog input value 
+  of specified pin (1 = read input and send message with value, 0 = dont read input and don't send message). 
+  
   Examples:
 
-  Digital write:
+  digital write:
 
-      To turn on digital pin 2, the string data received should be in the following format:
+      To turn on digital pin 2, the string data received by the Arduino should be in the following format:
 
              {digitalPin:2, state:1, pwm:0, getVal:0}
 
       pwm should be set to "0", state can either be 1(on) or 0(off)
 
  
-  Analog write:
+  analog write:
 
-      To write an pwm value of 150 to pin 3, the string data received should be in the following format:
+      To write a pwm value of 150 to pin 3, the string data received by the Arduino 
+      should be in the following format:
 
              {digitalPin:3, state:0, pwm:150, getVal:0}
 
-      state and getVal should always be set to "0".  PWM range is 0(full-off) to 255(full-on).
+      state and getVal should be set to "0".  PWM range is 0(full-off) to 255(full-on).
       use on Arduino pins D3,D5,D6,D9,D10,D11
 
  
-  Analog Read:
+  analog read:
 
-       To retrieve an analog value from analog A0, the string data should be in the following format:
+       To read an analog input value from analog A0 and to send a message back to the computer with the
+       value, the string data received by the Arduino should be in the following format:
 
              {analogPin:0, state:0, pwm:0, getVal:1}
 
        getVal should be set to 1, state and pwm should be set to 0.  
-       A value of 0-1023 will be returned in a message to the computer (see outgoing serial data section below. 
-       valid Arduino pins, A0-A5
+       A value of 0-1023 will be returned in a message back to the computer 
+       valid Arduino pins, A0-A5  See "Outgoing serial data" section below for more info 
 
 
   The examples above demonstrate sending string data in a highly verbose way, for easier readability.  
@@ -49,13 +53,13 @@
 
   IMPORTANT: The order of the number series in the string matters!
 
-  In the example above, the curley brackets are used to inidcate where to start parsing the data that has come into the receive buffer on the
-  Arduino, and where to stop.  The colons serve as parsing delimiters. The number after each colon delimiter is stored into an array, then
-  converted to an integer.
+  In the example above, the curley brackets are used to indicate when to start storing data into an array, and when to stop.  
+  The colons serve as parsing delimiters. The number after each colon delimiter is converted to an integer.
 
-  The first number in the series is used to specify the Arduino I/O pin.  The second number is used to set the state of the specified
-  digital pin (1-on or 0-off).  The third number is used to set to write a pmw value on the specifed pwm pin (0-255). The fourth number is used
-  to direct the Arduino to return an analog value from the specifed analog pin (1-to return the analog value or 0-not used).
+  The first number in the series is used to specify the Arduino I/O pin.  The second number is used to set the state of a specified
+  digital pin (1 = high or 0 = low).  The third number is used to write a pmw value (0-255) to the specifed pwm pin. The fourth number is used
+  to direct the Arduino to send a message about the current value of a specified analog input, (1 = read analog input and send message with the value,
+  0 = dont read an analog input and don't send a message).
 
 
 *********Outgoing serial data***************
@@ -64,11 +68,13 @@
 
   Examples of a typical format:
 
-     When a button is pressed on the Arduino, send string data out to the computer in the following format:
+     When a button is pressed on the Arduino, send a message out to the computer in the following format:
+        
         {message:vrs-button-press, value:true} value can either be true or false
 
-     When reading analog values from a potentiometer or sensor, send string data out to the computer in the following format:
-        {message:anythinghere, value:458} value returned can range from 0-1023
+     When reading analog values from a potentiometer or sensor, send a message out to the computer in the following format:
+        
+        {message:anythinghere, value:458} analog value returned can range from 0-1023
 */
 
 //global variables
@@ -77,9 +83,9 @@ const byte numChars = 32; // max number of characters the array can hold
 char receivedChars[numChars];
 char tempChars[numChars];
 int pin = 0; //physical I/O pin on Arduino
-int State = 0; //1=on, 0=off
+int State = 0; //1=high, 0=low
 int pwmval = 0; //pwm value, range 0-255
-int getVal = 0; //1 = return value, 0 = do not return value
+int getVal = 0; //1 = true, return value, 0 = false, do not return value
 boolean newData = false;
 bool handshake = false;
 long timeout = 0;
@@ -93,7 +99,7 @@ Button button1; //this creates an instance of "Button" class called button1.  Ad
 //Button button2; //this creates an instance of "Button" class called button2.  Add more buttons as needed.
 
 
-//This function reads incoming string data, and stores the characters bound by the start/end markers into an array
+//this function reads incoming string data, and stores the characters bound by the start/end markers into an array
 
 void recvWithStartEndMarkers() {
   static boolean recvInProgress = false;
@@ -125,7 +131,7 @@ void recvWithStartEndMarkers() {
 }
 
 
-//this function tokenizes string data and parses it into three parts, then converts the number text to integers
+//this function tokenizes string data and parses it into the four parts, then converts the number characters into integers
 
 void parseData() {    // split the string data into three parts
   char * strtokIndx; // this is used by strtok() as an index
@@ -143,7 +149,7 @@ void parseData() {    // split the string data into three parts
 
 }
 
-//this function writes digital pin states, pwm values to pwm pins, or returns analog values
+//this function writes digital pin states, pwm values to pwm pins, or returns analog input values
 
 void writePinState() {
   if (State == 1) digitalWrite(pin, State);
@@ -157,26 +163,29 @@ void writePinState() {
   }
 }
 
-//initialize serial, setup digital outputs, and setup buttons
 
-void setup() { //Setup Arduino I/O pins here
+//setup digital outputs and button inputs, attach pins to button functions, and initialize serial
+
+void setup() { //setup Arduino I/O pins here
 
   // Configure digital output pins here.  Analog inputs are already configured by default.
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP); //use INPUT_PULLUP) for buttons
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   
-
-
-  // Setup button inputs here
+  
+  //setup buttons here
   button1.setup(2, [](int state) {  //number after the "(" refers to the pin number the button is attached to
     if (state) Serial.println("{message:vrs-button-press, value:true}");  //put string data message between quotes;
   });
   //button2.setup(10, [](int state){  //number after the "(" refers to the pin number the button is attached to
   //  if (state) Serial.println("{message:anyname, value:true}");  //put string data message between quotes;
   //});
+  
 
-  Serial.begin(115200); //set serial baud rate to 9600
+  //setup serial
+  Serial.begin(115200); //set serial baud rate
+  //Serial.begin (9600); //set serial baud rate
   timeout = millis(); //get current time
   while (!Serial); //wait for serial port to open
   while (!handshake) { //loop here until valid handshake data has been sent by computer
@@ -190,6 +199,8 @@ void setup() { //Setup Arduino I/O pins here
     }
   }
 }
+
+
 
 //main Loop
 
