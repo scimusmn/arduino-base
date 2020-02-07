129ff16ef17ceaa0097fc0b134b21448d696c2c0
DBailey <dbailey@smm.org>
2020-02-07 08:59:04 -0600

    Added IntervalTme.h file

--- examples/IntervalTimerExample/IntervalTimerExample.ino
+++ examples/IntervalTimerExample/IntervalTimerExample.ino
@@ -3,7 +3,7 @@
 IntervalTmr intTimer1;
 
 void setup() {
- intTimer1 = IntervalTmr(13, 1000, 500); //pin number, on time, off time
+ intTimer1 = IntervalTmr(3, 1000, 500); //pin number, on time, off time
 }
 void loop() {
   intTimer1.enable();
