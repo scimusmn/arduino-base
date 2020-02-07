58a4e1609793c7dc278f02805995295bfde2c5c2
Brandon <brandonwkipp@gmail.com>
2020-02-07 09:53:31 -0600

    refactor: Updated syntax, added callback functionality, increased verbosity

---
+++ Libraries/FlipflopTimer.h
@@ -0,0 +1,41 @@
+#include "Arduino.h"
+
+// ensure this library description is only included once
+#ifndef FlipflopTimer_h
+#define FlipflopTimer_h
+
+class FlipflopTimer {
+private:
+  unsigned long onTime;
+  unsigned long offTime;
+  unsigned long nextTime = 0;
+public:
+  void (*callback)(boolean);
+  bool flipflopValue;
+
+  FlipflopTimer() {}
+
+  void setup(void (*CB)(bool), unsigned long duration1Time, unsigned long duration2Time) {
+    callback = CB;
+    onTime = duration1Time;
+    offTime = duration2Time;
+  }
+
+  void idle() {
+    unsigned long timeNow = millis();
+    if (timeNow >= nextTime) {
+      if (flpflopValue) {
+        flipflopValue = false;
+        nextTime = timeNow + offTime;
+      }
+      else {
+        flipflopValue = true;
+        nextTime = timeNow + onTime;
+      }
+
+      callback(flipflopValue);
+    }
+  }
+};
+
+#endif
