83070c468b7202746f59605e07f06f9fa652bf5d
Brandon <brandonwkipp@gmail.com>
2020-02-07 11:14:39 -0600

    refactor: Replaced IntervalTmr example sketch with new code, deleted original IntervalTmr sketch

---
+++ examples/FlipflopTimerExample/FlipflopTimerExample.ino
@@ -0,0 +1,12 @@
+#include "arduino-base/Libraries/FlipflopTimer.h"
+
+FlipflopTimer flipflopTimer1;
+
+void setup() {
+  flipflopTimer1.setup([](boolean flipflopValue) {
+    digitalWrite(ledPin, flipflopValue);
+  }, 1000, 500);
+}
+void loop() {
+  flipflopTimer1.idle();
+}
