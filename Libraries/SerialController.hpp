2e696cf01f8dd31a1cb0a565221f873c0f57e1a0
Joe <jmeyer@smm.org>
2020-02-11 16:37:31 -0600

    fix for multiple includes

--- Libraries/SerialController.hpp
+++ Libraries/SerialController.hpp
@@ -28,8 +28,6 @@ class SerialController {
   char value[MAX_STRING_LEN];
   int keyIndex, valueIndex;
 
-  void waitForSerial(long baudrate);
-
  public:
   bool handshake;
 
@@ -39,14 +37,154 @@ class SerialController {
     callback = callback_;
   }
 
-  void sendMessage(char* messageKey, char* messageValue);
-  void sendMessage(char* messageKey, int messageValue);
-  void sendMessage(char* messageKey, unsigned int messageValue);
-  void sendMessage(char* messageKey, long int messageValue);
-  void sendMessage(char* messageKey, long unsigned int messageValue);
-  void sendMessage(char* messageKey, float messageValue);
+  void waitForSerial(long baudrate) {
+    Serial.begin(baudrate);
+    while(!Serial);
 
-  void update();
+    while(!handshake) {
+      if (Serial.available() && Serial.read() == '{') {
+        handshake = true;
+        sendMessage("arduino-ready","1");
+      }
+    }
+  }
+
+  // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  void sendMessage(char* messageKey, char* messageValue) {
+    cleanString(messageKey);
+    cleanString(messageValue);
+
+    char result[2*MAX_STRING_LEN+3];
+    strcpy(result,"{");
+    strcat(result,messageKey);
+    strcat(result,":");
+    strcat(result,messageValue);
+    strcat(result,"}");
+
+    Serial.println(result);
+  }
+
+  void sendMessage(char* messageKey, int messageValue) {
+    char stringValue[MAX_STRING_LEN];
+    snprintf(stringValue, MAX_STRING_LEN, "%d", messageValue);
+    sendMessage(messageKey, stringValue);
+  }
+
+  void sendMessage(char* messageKey, unsigned int messageValue) {
+    char stringValue[MAX_STRING_LEN];
+    snprintf(stringValue, MAX_STRING_LEN, "%u", messageValue);
+    sendMessage(messageKey, stringValue);
+  }
+
+  void sendMessage(char* messageKey, long int messageValue) {
+    char stringValue[MAX_STRING_LEN];
+    snprintf(stringValue, MAX_STRING_LEN, "%ld", messageValue);
+    sendMessage(messageKey, stringValue);
+  }
+
+  void sendMessage(char* messageKey, long unsigned int messageValue) {
+    char stringValue[MAX_STRING_LEN];
+    snprintf(stringValue, MAX_STRING_LEN, "%lu", messageValue);
+    sendMessage(messageKey, stringValue);
+  }
+
+  void sendMessage(char* messageKey, float messageValue) {
+    char stringValue[MAX_STRING_LEN];
+    snprintf(stringValue, MAX_STRING_LEN, "%f", messageValue);
+    sendMessage(messageKey, stringValue);
+  }
+
+  void update() {
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+      switch (state) {
+      case WAIT_FOR_START:
+        if (c == '{') {
+          state = PARSE_KEY;
+          strcpy(key,"");
+          strcpy(value,"");
+          keyIndex = 0;
+          valueIndex = 0;
+        }
+        break;
+
+      case PARSE_KEY:
+        if (keyIndex == MAX_STRING_LEN-1) {
+          key[keyIndex] = 0;
+          state = PARSE_KEY_OVERFLOW;
+        }
+        else if (c == '{') {
+          strcpy(key,"");
+          strcpy(value,"");
+          keyIndex = 0;
+          valueIndex = 0;
+        }
+        else if (c == ':') {
+          key[keyIndex] = 0;
+          state = PARSE_VALUE;
+        }
+        else if (c == '}') {
+          key[keyIndex] = 0;
+          (*callback)(key,value);
+          state = WAIT_FOR_START;
+        }
+        else {
+          key[keyIndex] = c;
+          keyIndex++;
+        }
+        break;
+
+      case PARSE_VALUE:
+        if (valueIndex == MAX_STRING_LEN - 1) {
+          value[valueIndex] = 0;
+          state = PARSE_VALUE_OVERFLOW;
+        }
+        else if (c == '}') {
+          value[valueIndex] = 0;
+          (*callback)(key,value);
+          state = WAIT_FOR_START;
+        }
+        else {
+          value[valueIndex] = c;
+          valueIndex++;
+        }
+        break;
+
+      case PARSE_KEY_OVERFLOW:
+        if (c == ':') {
+          state = PARSE_VALUE;
+        }
+        else if (c == '{') {
+          strcpy(key,"");
+          strcpy(value,"");
+          keyIndex = 0;
+          valueIndex = 0;
+          state = PARSE_KEY;
+        }
+        break;
+
+      case PARSE_VALUE_OVERFLOW:
+        if (c == '{') {
+          strcpy(key,"");
+          strcpy(value,"");
+          keyIndex = 0;
+          valueIndex = 0;
+          state = PARSE_KEY;
+        }
+        else if (c == '}') {
+          (*callback)(key,value);
+          state = WAIT_FOR_START;
+        }
+        break;
+
+      default:
+        // something's gone wrong, reset
+        state = WAIT_FOR_START;
+        break;
+      }
+    }
+  }
 };
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -74,160 +212,5 @@ void cleanString(char* string) {
 
   string = result;
 }
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::waitForSerial(long baudrate) {
-  Serial.begin(baudrate);
-  while(!Serial);
-
-  while(!handshake) {
-    if (Serial.available() && Serial.read() == '{') {
-      handshake = true;
-      sendMessage("arduino-ready","1");
-    }
-  }
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::sendMessage(char* messageKey, char* messageValue) {
-  cleanString(messageKey);
-  cleanString(messageValue);
-
-  char result[2*MAX_STRING_LEN+3];
-  strcpy(result,"{");
-  strcat(result,messageKey);
-  strcat(result,":");
-  strcat(result,messageValue);
-  strcat(result,"}");
-
-  Serial.println(result);
-}
-
-void SerialController::sendMessage(char* messageKey, int messageValue) {
-  char stringValue[MAX_STRING_LEN];
-  snprintf(stringValue, MAX_STRING_LEN, "%d", messageValue);
-  sendMessage(messageKey, stringValue);
-}
-
-void SerialController::sendMessage(char* messageKey, unsigned int messageValue) {
-  char stringValue[MAX_STRING_LEN];
-  snprintf(stringValue, MAX_STRING_LEN, "%u", messageValue);
-  sendMessage(messageKey, stringValue);
-}
-
-void SerialController::sendMessage(char* messageKey, long int messageValue) {
-  char stringValue[MAX_STRING_LEN];
-  snprintf(stringValue, MAX_STRING_LEN, "%ld", messageValue);
-  sendMessage(messageKey, stringValue);
-}
-
-void SerialController::sendMessage(char* messageKey, long unsigned int messageValue) {
-  char stringValue[MAX_STRING_LEN];
-  snprintf(stringValue, MAX_STRING_LEN, "%lu", messageValue);
-  sendMessage(messageKey, stringValue);
-}
-
-void SerialController::sendMessage(char* messageKey, float messageValue) {
-  char stringValue[MAX_STRING_LEN];
-  snprintf(stringValue, MAX_STRING_LEN, "%f", messageValue);
-  sendMessage(messageKey, stringValue);
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::update() {
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    switch (state) {
-    case WAIT_FOR_START:
-      if (c == '{') {
-        state = PARSE_KEY;
-        strcpy(key,"");
-        strcpy(value,"");
-        keyIndex = 0;
-        valueIndex = 0;
-      }
-      break;
-
-    case PARSE_KEY:
-      if (keyIndex == MAX_STRING_LEN-1) {
-        key[keyIndex] = 0;
-        state = PARSE_KEY_OVERFLOW;
-      }
-      else if (c == '{') {
-        strcpy(key,"");
-        strcpy(value,"");
-        keyIndex = 0;
-        valueIndex = 0;
-      }
-      else if (c == ':') {
-        key[keyIndex] = 0;
-        state = PARSE_VALUE;
-      }
-      else if (c == '}') {
-        key[keyIndex] = 0;
-        (*callback)(key,value);
-        state = WAIT_FOR_START;
-      }
-      else {
-        key[keyIndex] = c;
-        keyIndex++;
-      }
-      break;
-
-    case PARSE_VALUE:
-      if (valueIndex == MAX_STRING_LEN - 1) {
-        value[valueIndex] = 0;
-        state = PARSE_VALUE_OVERFLOW;
-      }
-      else if (c == '}') {
-        value[valueIndex] = 0;
-        (*callback)(key,value);
-        state = WAIT_FOR_START;
-      }
-      else {
-        value[valueIndex] = c;
-        valueIndex++;
-      }
-      break;
-
-    case PARSE_KEY_OVERFLOW:
-      if (c == ':') {
-        state = PARSE_VALUE;
-      }
-      else if (c == '{') {
-        strcpy(key,"");
-        strcpy(value,"");
-        keyIndex = 0;
-        valueIndex = 0;
-        state = PARSE_KEY;
-      }
-      break;
-
-    case PARSE_VALUE_OVERFLOW:
-      if (c == '{') {
-        strcpy(key,"");
-        strcpy(value,"");
-        keyIndex = 0;
-        valueIndex = 0;
-        state = PARSE_KEY;
-      }
-      else if (c == '}') {
-        (*callback)(key,value);
-        state = WAIT_FOR_START;
-      }
-      break;
-
-    default:
-      // something's gone wrong, reset
-      state = WAIT_FOR_START;
-      break;
-    }
-  }
-}
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #endif
