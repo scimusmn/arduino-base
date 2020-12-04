9eb8a90092a8f87fc53560f6a926f039f9bdf87a
tnordberg <tnordberg@smm.org>
2020-12-04 09:46:16 -0600

    Separating example from gatsby-centric wrapper page.

---
+++ ReactSerial/examples/gatsby/gatsby-wrapper-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ArduinoTestPage from '../Arduino/arduino-base/ReactSerial/examples/arduino-test-page';
+
+const ArduinoWrapperPage = () => (
+  <ArduinoTestPage />
+);
+
+export default ArduinoWrapperPage;
