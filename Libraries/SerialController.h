94f8ef22a75988978fd921830ba4bd0e3b2358d0
sanine-a <sanine.not@pm.me>
2021-05-27 13:46:52 -0500

    update to used fixed-size strings

--- Libraries/SerialController.h
+++ Libraries/SerialController.h
@@ -1,14 +1,68 @@
 #ifndef SERIAL_CONTROLLER_H
 #define SERIAL_CONTROLLER_H
 
-#define MAX_STRING_LEN 128
-
 #include <Arduino.h>
+#include <string.h>
 
-typedef void (*stringCallback)(String);
+#ifndef MAX_KEY_LEN
+#define MAX_KEY_LEN 32
+#endif
+
+#ifndef MAX_VAL_LEN
+#define MAX_VAL_LEN 32
+#endif
+
+#define MAX_MSG_LEN (MAX_KEY_LEN + MAX_VAL_LEN + 3)
+
+typedef void (*voidCallback)();
+typedef void (*stringCallback)(char*);
 typedef void (*intCallback)(int);
 typedef void (*floatCallback)(float);
 
+struct StringF {
+  int maxSize;
+  int size;
+  char *str;
+
+  StringF(int maxSize) : maxSize(maxSize), size(0) {
+    str = malloc(maxSize * sizeof(char));
+  }
+
+  ~StringF() { free(str); }
+
+  void clear() {
+    memset(str, 0, maxSize);
+    size = 0;
+  }
+
+  void append(char *string) {
+    int s = snprintf(str, maxSize, "%s%s", str, string);
+    size = s >= maxSize ? maxSize-1 : s;
+  }
+
+  void append(char c) {
+    if (size < maxSize-1) {
+      str[size] = c;
+      size++;
+    }
+  }
+
+  int toInt() {
+    return atoi(str);
+  }
+
+  float toFloat() {
+    return atof(str);
+  }
+
+  bool equals(StringF string) {
+    return strcmp(str, string.str) == 0;
+  }
+
+  bool equals(char *string) {
+    return strcmp(str, string) == 0;
+  }
+};
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *
@@ -18,9 +72,12 @@ typedef void (*floatCallback)(float);
  */
 
 struct SerialCallback {
-  String key;
+  StringF key;
+
+  SerialCallback() : key(MAX_KEY_LEN) {}
 
   typedef enum {
+    VOID,
     STRING,
     INT,
     FLOAT
@@ -29,34 +86,36 @@ struct SerialCallback {
   ValueType valueType;
 
   union {
+    voidCallback v;
     stringCallback s;
     intCallback i;
     floatCallback f;
   } callback;
 
-  void respond(String value);
-};
+  void respond(StringF value) {
+    switch (valueType) {
+    case VOID:
+      callback.v();
+      break;
+      
+    case STRING:
+      callback.s(value.str);
+      break;
 
+    case INT:
+      callback.i(value.toInt());
+      break;
 
-void SerialCallback::respond(String value) {
-  switch (valueType) {
-  case STRING:
-    callback.s(value);
-    break;
+    case FLOAT:
+      callback.f(value.toFloat());
+      break;
 
-  case INT:
-    callback.i(value.toInt());
-    break;
-
-  case FLOAT:
-    callback.f(value.toFloat());
-    break;
-
-  default:
-    // bad valueType, do nothing
-    break;
+    default:
+      // bad valueType, do nothing
+      break;
+    }
   }
-}
+};
 
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -70,6 +129,16 @@ void SerialCallback::respond(String value) {
 #define SERIALCONTROLLER_MAX_CALLBACKS 16
 #endif
 
+// helper to define repetitious addCallback overloads
+#define addCallbackDef(cbType, valType, u)				\
+  void addCallback(const char *keyString, cbType callback) {		\
+    SerialCallback *cb = callbacks + numCallbacks;			\
+    cb->key.str = keyString;						\
+    cb->valueType = SerialCallback::ValueType::valType;			\
+    cb->callback.u = callback;						\
+    numCallbacks++;							\
+  }
+
 class SerialController {
 private:
   enum {
@@ -82,195 +151,138 @@ private:
 
   int numCallbacks;
   SerialCallback callbacks[SERIALCONTROLLER_MAX_CALLBACKS];
-  String key, value;
+  StringF key;
+  StringF value;
   bool steleProtocol;
 
-  void waitForSerial(long baudrate);
+  void waitForSerial(long baudrate) {
+    Serial.begin(baudrate);
+    while(!Serial);
 
-  void cleanString(String& string);
+    // wait for handshake if using steleProtocol
+    while(!handshake && steleProtocol) {
+      if (Serial.available() && Serial.read() == '{') {
+	handshake = true;
+	sendMessage("arduino-ready","1");
+      }
+    }
+  }
 
-  void lookupAndRunCallback();
 
+  void lookupAndRunCallback() {
+    if (steleProtocol && key.equals("wake-arduino")) {
+      sendMessage("arduino-ready", "1");
+      return;
+    }
+
+    for (int i=0; i<numCallbacks; i++) {
+      Serial.println(callbacks[i].key.str);
+      if (key.equals(callbacks[i].key)) {
+	callbacks[i].respond(value);
+	return;
+      }
+    }
+
+    if (errorResponse) {
+      sendMessage("unknown-command", key.str);
+    }
+  }
+
+  
 public:
   bool handshake;
   bool errorResponse;
 
-  SerialManager() { state = WAIT_FOR_START; numCallbacks = 0; }
+  SerialController() : key(MAX_KEY_LEN), value(MAX_VAL_LEN) { state = WAIT_FOR_START; numCallbacks = 0; }
 
-  void setup(long baudrate=115200, bool steleProtocol=true);
+  void setup(long baudrate=115200, bool steleProtocol=true) {
+    this->steleProtocol = steleProtocol;
+    errorResponse = true;
+    waitForSerial(baudrate);
+  }
 
-  void addCallback(String key, stringCallback callback);
-  void addCallback(String key, intCallback callback);
-  void addCallback(String key, floatCallback callback);
   
-  void sendMessage(String messageKey, String messageValue);
-  void sendMessage(String messageKey, int messageValue);
-  void sendMessage(String messageKey, float messageValue);
+  // overloaded callback-adding functions
+  addCallbackDef(voidCallback, VOID, v)
+  addCallbackDef(stringCallback, STRING, s)
+  addCallbackDef(intCallback, INT, i)
+  addCallbackDef(floatCallback, FLOAT, f)
 
-  void update();
+  
+  // message sending
+  void sendMessage(char *messageKey, char *messageValue) {
+    char msg[MAX_MSG_LEN];
+    int size = snprintf(msg, MAX_MSG_LEN, "{%s:%s}", messageKey, messageValue);
+    if (size > MAX_MSG_LEN)
+      Serial.println("{message-too-big:1}");
+    else
+      Serial.println(msg);
+  }
+
+  void sendMessage(char *messageKey) {
+    sendMessage(messageKey, "1");
+  }
+
+  void sendMessage(char *messageKey, int messageValue) {
+    char value[MAX_VAL_LEN];
+    snprintf(value, MAX_VAL_LEN, "%d", messageValue);
+    sendMessage(messageKey, value);
+  }
+
+
+  void sendMessage(char *messageKey, float messageValue) {
+    char value[MAX_VAL_LEN];
+    snprintf(value, MAX_VAL_LEN, "%f", messageValue);
+    sendMessage(messageKey, value);
+  }
+
+
+  // updating
+  void update() {
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+      switch (state) {
+      case WAIT_FOR_START:
+	if (c == '{') {
+	  key.clear();
+	  value.clear();
+	  state = PARSE_KEY;
+	}
+	break;
+
+      case PARSE_KEY:
+	if (c == '{') {
+	  state = PARSE_KEY;
+	}
+	else if (c == ':') {
+	  state = PARSE_VALUE;
+	}
+	else if (c == '}') {
+	  // malformed input, look for next token
+	  state = WAIT_FOR_START;
+	}
+	else {
+	  key.append(c);
+	}
+	break;
+
+      case PARSE_VALUE:
+	if (c == '}') {
+	  lookupAndRunCallback();
+	  state = WAIT_FOR_START;
+	}
+	else {
+	  value.append(c);
+	}
+	break;
+
+      default:
+	// something's gone wrong, reset
+	state = WAIT_FOR_START;
+	break;
+      }
+    }
+  }
 };
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::setup(long baudrate, bool steleProtocol) {
-  this->steleProtocol = steleProtocol;
-  errorResponse = true;
-  waitForSerial(baudrate);
-}
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::addCallback(String key, stringCallback callback) {
-  SerialCallback* cb = callbacks + numCallbacks;
-
-  cb->key = key;
-  cb->valueType = SerialCallback::ValueType::STRING;
-  cb->callback.s = callback;
-  
-  numCallbacks += 1;
-}
-
-
-void SerialController::addCallback(String key, intCallback callback) {
-  SerialCallback* cb = callbacks + numCallbacks;
-
-  cb->key = key;
-  cb->valueType = SerialCallback::ValueType::INT;
-  cb->callback.i = callback;
-
-  numCallbacks += 1;
-}
-
-
-void SerialController::addCallback(String key, floatCallback callback) {
-  SerialCallback* cb = callbacks + numCallbacks;
-
-  cb->key = key;
-  cb->valueType = SerialCallback::ValueType::FLOAT;
-  cb->callback.f = callback;
-
-  numCallbacks += 1;
-}
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::sendMessage(String messageKey, String messageValue) {
-  cleanString(messageKey);
-  cleanString(messageValue);
-
-  String result = "{";
-  result.concat(messageKey);
-  result.concat(":");
-  result.concat(messageValue);
-  result.concat("}");
-
-  Serial.println(result);
-}
-
-
-void SerialController::sendMessage(String messageKey, int messageValue) {
-  sendMessage(messageKey, String(messageValue));
-}
-
-
-void SerialController::sendMessage(String messageKey, float messageValue) {
-  sendMessage(messageKey, String(messageValue));
-}
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::update() {
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    switch (state) {
-    case WAIT_FOR_START:
-      if (c == '{') {
-	key = "";
-	value = "";
-	state = PARSE_KEY;
-      }
-      break;
-
-    case PARSE_KEY:
-      if (c == '{') {
-	key = "";
-	value = "";
-	state = PARSE_KEY;
-      }
-      else if (c == ':') {
-	state = PARSE_VALUE;
-      }
-      else if (c == '}') {
-	lookupAndRunCallback();
-	state = WAIT_FOR_START;
-      }
-      else {
-	key.concat(c);
-      }
-      break;
-
-    case PARSE_VALUE:
-      if (c == '}') {
-	lookupAndRunCallback();
-	state = WAIT_FOR_START;
-      }
-      else {
-	value.concat(c);
-      }
-      break;
-
-    default:
-      // something's gone wrong, reset
-      state = WAIT_FOR_START;
-      break;
-    }
-  }
-}
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-void SerialController::waitForSerial(long baudrate) {
-  Serial.begin(baudrate);
-  while(!Serial);
-
-  // wait for handshake if using steleProtocol
-  while(!handshake && steleProtocol) {
-    if (Serial.available() && Serial.read() == '{') {
-      handshake = true;
-      sendMessage("arduino-ready","1");
-    }
-  }
-}
-
-
-void SerialController::cleanString(String& string) {
-  string.replace("{", "");
-  string.replace("}", "");
-  string.replace(":", "");
-}
-
-
-void SerialController::lookupAndRunCallback() {
-  if (steleProtocol && key == "wake-arduino") {
-    sendMessage("arduino-ready", "1");
-    return;
-  }
-
-  for (int i=0; i<numCallbacks; i++) {
-    if (key == callbacks[i].key) {
-      callbacks[i].respond(value);
-      return;
-    }
-  }
-
-  if (errorResponse) {
-    sendMessage("unknown-command", key);
-  }
-}
 
 #endif
