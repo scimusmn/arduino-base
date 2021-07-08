26480edf7313539863749b3389903d677999092e
sanine-a <sanine.not@pm.me>
2021-07-08 13:30:26 -0500

    add doxygen comments and initial function skeletons for LookupTable

--- Libraries/LookupTable.h
+++ Libraries/LookupTable.h
@@ -7,18 +7,17 @@ namespace smm {
     template<unsigned int MAX_ENTRIES, typename T, size_t MAX_STR_LEN = 32>
     class LookupTable {
     public:
-	LookupTable() {}
-
 	/** look up a particular entry in the table.
 	 *
 	 * @param[in] key The key for the entry to look up.
 	 *
 	 * @returns A pointer to the requested entry if it exists, and `nullptr` otherwise.
-	 *
+	 */
 	T* operator[](const char *key) {
 	    return nullptr;
 	}
 
+	
 	/** look up a particular entry in the table.
 	 *
 	 * This is an overload of the above function and differs only in the arguments it accepts.
@@ -26,14 +25,39 @@ namespace smm {
 	 * @param[in] key The key for the entry to look up.
 	 *
 	 * @returns A pointer to the requested entry if it exists, and `nullptr` otherwise.
+	 */
+	T* operator[](smm::FixedSizeString<MAX_STR_LEN>& key) { return nullptr; }
+
+	
+	/** add an entry to the table
 	 *
-	T* operator[](smm::FixedSizeString& key) { return this->operator[](key.c_str()); }
-	*/
-
-	/*bool addEntry(smm::FixedSizeString& key, T value);*/
+	 * @param[in] key The key for the new entry
+	 * @param[in] value The value for the new entry
+	 *
+	 * @returns True if the entry was added successfully and false otherwise. */
+	bool addEntry(const char *key, T value) { return false; }
 
 	
+	/** add an entry to the table
+	 * This is an overload of the above function and differs only in the arguments it accepts.
+	 * 
+	 * @param[in] key The key for the new entry
+	 * @param[in] value The value for the new entry
+	 *
+	 * @returns True if the entry was added successfully and false otherwise. */
+	bool addEntry(smm::FixedSizeString<MAX_STR_LEN>& key, T value) { return false; }
+
+
+	/** get the current number of table entries
+	 *
+	 * @returns The number of entries currently in the table. */
+	unsigned int size() { return m_numEntries; }
 	
+
+	/** get the maximum possible number of table entries
+	 *
+	 * @returns The maximum possible number of entries the table can hold. */
+	unsigned int maxSize() { return MAX_ENTRIES; }
 
     private:
 	unsigned int m_numEntries;
