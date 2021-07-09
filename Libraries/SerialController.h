81bdc6fda1cdc0cb7ac815897a6208c1d4e89129
sanine-a <sanine.not@pm.me>
2021-07-09 15:58:02 -0500

    create SerialController tests for malformed inputs

--- Libraries/SerialController.h
+++ Libraries/SerialController.h
@@ -9,126 +9,107 @@
 #include "FixedSizeString.h"
 #include "LookupTable.h"
 
-#ifndef MAX_KEY_LEN
-#define MAX_KEY_LEN 32
-#endif
+namespace smm {
 
-#ifndef MAX_VAL_LEN
-#define MAX_VAL_LEN 32
-#endif
-
-#define MAX_MSG_LEN (MAX_KEY_LEN + MAX_VAL_LEN + 3)
-
-typedef void (*voidCallback)();
-typedef void (*stringCallback)(char*);
-typedef void (*intCallback)(int);
-typedef void (*floatCallback)(float);
+    typedef void (*voidCallback)();
+    typedef void (*stringCallback)(const char*);
+    typedef void (*intCallback)(int);
+    typedef void (*floatCallback)(float);
 
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *
- * SerialCallback
- *
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * SerialCallback
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
-template<size_t MAX_STR_LEN>
-struct SerialCallback {
-    SerialCallback(voidCallback cb)   { valueType = VOID;   callback.v = cb; }
-    SerialCallback(stringCallback cb) { valueType = STRING; callback.s = cb; }
-    SerialCallback(intCallback cb)    { valueType = INT;    callback.i = cb; }
-    SerialCallback(floatCallback cb)  { valueType = FLOAT;  callback.f = cb; }
+    struct SerialCallback {
+	SerialCallback(voidCallback cb)   { valueType = VOID;   callback.v = cb; }
+	SerialCallback(stringCallback cb) { valueType = STRING; callback.s = cb; }
+	SerialCallback(intCallback cb)    { valueType = INT;    callback.i = cb; }
+	SerialCallback(floatCallback cb)  { valueType = FLOAT;  callback.f = cb; }
 
-    typedef enum {
-	VOID,
-	STRING,
-	INT,
-	FLOAT
-    } ValueType;
+	typedef enum {
+	    VOID,
+	    STRING,
+	    INT,
+	    FLOAT
+	} ValueType;
 
-    ValueType valueType;
+	ValueType valueType;
 
-    union {
-	voidCallback v;
-	stringCallback s;
-	intCallback i;
-	floatCallback f;
-    } callback;
+	union {
+	    voidCallback v;
+	    stringCallback s;
+	    intCallback i;
+	    floatCallback f;
+	} callback;
 
-    void operator()(smm::FixedSizeString<MAX_STR_LEN>& value) {
-	switch (valueType) {
-	case VOID:
-	    callback.v();
-	    break;
+	void operator()(const char *value) {
+	    switch (valueType) {
+	    case VOID:
+		callback.v();
+		break;
       
-	case STRING:
-	    callback.s(value.c_str());
-	    break;
+	    case STRING:
+		callback.s(value);
+		break;
 
-	case INT:
-	    callback.i(value.toInt());
-	    break;
+	    case INT:
+		callback.i(atoi(value));
+		break;
 
-	case FLOAT:
-	    callback.f(value.toFloat());
-	    break;
+	    case FLOAT:
+		callback.f(atof(value));
+		break;
 
-	default:
-	    // bad valueType, do nothing
-	    break;
+	    default:
+		// bad valueType, do nothing
+		break;
+	    }
 	}
-    }
-};
+    };
 
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *
- * SerialController
- *
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     *
+     * SerialController
+     *
+     * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     */
 
-#ifndef SERIALCONTROLLER_MAX_CALLBACKS
-#define SERIALCONTROLLER_MAX_CALLBACKS 16
-#endif
+    #define NORMAL_PROTOCOL 0
+    #define STELE_PROTOCOL 1
 
-// helper to define repetitious addCallback overloads
-#define addCallbackDef(cbType, valType, u)			\
-    void addCallback(const char *keyString, cbType callback) {	\
-	SerialCallback *cb = callbacks + numCallbacks;		\
-	cb->key.str = keyString;				\
-	cb->valueType = SerialCallback::ValueType::valType;	\
-	cb->callback.u = callback;				\
-	numCallbacks++;						\
-    }
-
-class SerialController {
-private:
+    template<int PROTOCOL=NORMAL_PROTOCOL, unsigned int MAX_CALLBACKS=16,
+	     size_t MAX_KEY_LEN=32, size_t MAX_VAL_LEN=MAX_KEY_LEN>
+    class SerialController {
+    public:
+	void setup(long baudrate=115200) {}
   
-public:
-    bool handshake;
-    bool errorResponse;
-
-    void setup(long baudrate=115200, bool steleProtocol=true) {}
+	// overloaded callback-adding functions
+	void addCallback(const char *key, voidCallback cb) {}
+	void addCallback(const char *key, stringCallback cb) {}
+	void addCallback(const char *key, intCallback cb) {}
+	void addCallback(const char *key, floatCallback cb) {}
   
-    // overloaded callback-adding functions
-    /*addCallbackDef(voidCallback, VOID, v)
-    addCallbackDef(stringCallback, STRING, s)
-    addCallbackDef(intCallback, INT, i)
-    addCallbackDef(floatCallback, FLOAT, f)*/
-
-  
-    // message sending
-    void sendMessage(char *messageKey, char *messageValue) {}
-
-    void sendMessage(char *messageKey) {}
-
-    void sendMessage(char *messageKey, int messageValue) {}
+	// message sending
+	void sendMessage(const char *messageKey, const char *messageValue) {}
+	void sendMessage(const char *messageKey) {}
+	void sendMessage(const char *messageKey, int messageValue) {}
+	void sendMessage(const char *messageKey, float messageValue) {}
 
 
-    void sendMessage(char *messageKey, float messageValue) {}
+	// updating
+	void update() {}
 
 
-    // updating
-    void update() {}
-};
+	// size info
+	size_t maxKeyLength() { return MAX_KEY_LEN; }
+	size_t maxValueLength() { return MAX_VAL_LEN; }
+	unsigned int maxNumCallbacks() { return MAX_CALLBACKS; }
+
+	unsigned int numCallbacks() { return 0; }
+    };
+}
--- Libraries/tests/SerialControllerTests.cpp
+++ Libraries/tests/SerialControllerTests.cpp
@@ -1,8 +1,275 @@
 #include "tests.h"
 #include "../SerialController.h"
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * SerialCallback tests
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+bool success = false;
+const char *str = "";
+int integer = 0;
+float number = 0.0f;
+
+void voidcb() { success = true; }
+void stringcb(const char *string) { str = string; }
+void intcb(int n) { integer = n; }
+void floatcb(float f) { number = f; }
+
+
+mu_test scb_void() {
+    smm::SerialCallback v(voidcb);
+    mu_assert_equal(v.callback.v, voidcb);
+    v("literally anything");
+    mu_assert_equal(success, true);
+    return 0;
+}
+
+
+mu_test scb_string() {
+    smm::SerialCallback s(stringcb);
+    mu_assert_equal(s.callback.s, stringcb);
+    s("hello!");
+    mu_assert_streq(str, "hello!");
+    return 0;
+}
+
+
+mu_test scb_int() {
+    smm::SerialCallback i(intcb);
+    mu_assert_equal(i.callback.i, intcb);
+    i("42");
+    mu_assert_equal(integer, 42);
+    return 0;
+}
+
+
+mu_test scb_float() {
+    smm::SerialCallback f(floatcb);
+    mu_assert_equal(f.callback.f, floatcb);
+    f("1.618f");
+    mu_assert_equal(number, 1.618f);
+    return 0;
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * SerialController tests
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#define SERIAL_RESET() do {			\
+	Serial.reset();				\
+	success = false;			\
+	str = "";				\
+	integer = 0;				\
+	number = 0.0f;				\
+    } while(0)
+
+
+#define STANDARD_SETUP()				\
+    smm::SerialController<> controller;			\
+    mu_assert_equal(controller.maxNumCallbacks(), 16);	\
+    mu_assert_equal(controller.numCallbacks(), 0);	\
+    mu_assert_equal(controller.maxKeyLength(), 32);	\
+    mu_assert_equal(controller.maxValueLength(), 32);	\
+    controller.setup();					\
+    controller.addCallback("void", voidcb);		\
+    controller.addCallback("string", stringcb);		\
+    controller.addCallback("int", intcb);		\
+    controller.addCallback("float", floatcb);		\
+    mu_assert_equal(controller.numCallbacks(), 4);
+
+
+mu_test sctrl_normal() {
+    STANDARD_SETUP();
+
+    SERIAL_RESET();
+    Serial.send("{void:1}");
+    controller.update();
+    mu_assert_equal(success, true);
+
+    SERIAL_RESET();
+    Serial.send("{string:hello}");
+    controller.update();
+    mu_assert_equal(str, "hello");
+
+    SERIAL_RESET();
+    Serial.send("{int:44}");
+    controller.update();
+    mu_assert_equal(integer, 44);
+
+    SERIAL_RESET();
+    Serial.send("{float:2.718}");
+    controller.update();
+    mu_assert_equal(number, 2.718f);
+
+    return 0;
+}
+
+
+mu_test sctrl_overflow() {
+    smm::SerialController<NORMAL_PROTOCOL, 2, 4> controller;
+
+    mu_assert_equal(controller.maxNumCallbacks(), 2);
+    mu_assert_equal(controller.numCallbacks(), 0);
+    mu_assert_equal(controller.maxKeyLength(), 4);
+    mu_assert_equal(controller.maxValueLength(), 4);
+
+    controller.setup();
+    controller.addCallback("void", voidcb);
+    controller.addCallback("string", stringcb);
+    controller.addCallback("int", intcb);
+    controller.addCallback("float", floatcb);
+
+    mu_assert_equal(controller.numCallbacks(), 2);
+
+    SERIAL_RESET();
+    Serial.send("{void:1}");
+    controller.update();
+    mu_assert_equal(success, true);
+
+    SERIAL_RESET();
+    Serial.send("{string:hello}");
+    controller.update();
+    mu_assert_unequal(str, "hello");
+    mu_assert_equal(str, "");
+    SERIAL_RESET();
+    Serial.send("{stri:hello}");
+    controller.update();
+    mu_assert_equal(str, "hell");
+
+    SERIAL_RESET();
+    Serial.send("{int:44}");
+    controller.update();
+    mu_assert_unequal(integer, 44);
+
+    SERIAL_RESET();
+    Serial.send("{float:2.718}");
+    controller.update();
+    mu_assert_unequal(number, 2.718f);
+
+    return 0;
+}
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * malformed input tests
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+mu_test sctrl_malformed_closingbracket() {
+    STANDARD_SETUP();
+
+    // controller should ignore messages with missing closing bracket
+    SERIAL_RESET();
+    Serial.send("{string:1");
+    controller.update();
+    mu_assert_equal(str, "");
+    Serial.send("{string:b}");
+    controller.update();
+    mu_assert_unequal(str, "1{string:b");
+    mu_assert_equal(str, "b");
+
+    return 0;
+}
+
+
+mu_test sctrl_malformed_openingbracket() {
+    STANDARD_SETUP();
+
+    // controller should ignore messages with missing opening bracket
+    SERIAL_RESET();
+    Serial.send("string:1}");
+    controller.update();
+    mu_assert_equal(str, "");
+    Serial.send("{string:b}");
+    controller.update();
+    mu_assert_equal(str, "b");
+
+    return 0;
+}
+
+
+mu_test sctrl_malformed_colon() {
+    STANDARD_SETUP();
+
+    // controller should ignore messages with missing colon
+    SERIAL_RESET();
+    Serial.send("{string1}");
+    controller.update();
+    mu_assert_equal(str, "");
+    Serial.send("{string:b}");
+    controller.update();
+    mu_assert_equal(str, "b");
+
+    return 0;
+}
+
+
+mu_test sctrl_malformed_multi_closingbracket() {
+    STANDARD_SETUP();
+
+    // controller should process messages with multiple closing brackets
+    SERIAL_RESET();
+    Serial.send("{string:b}}}");
+    controller.update();
+    mu_assert_equal(str, "b");
+
+    return 0;
+}
+
+
+mu_test sctrl_malformed_multi_openingbracket() {
+    STANDARD_SETUP();
+
+    // controller should process messages with multiple opening brackets
+    SERIAL_RESET();
+    Serial.send("{{{string:b}");
+    controller.update();
+    mu_assert_equal(str, "b");
+
+    return 0;
+}
+
+
+mu_test sctrl_malformed_multi_colon() {
+    STANDARD_SETUP();
+
+    // controller should ignore messages with multiple colons
+    SERIAL_RESET();
+    Serial.send("{string:b:q:p}");
+    controller.update();
+    mu_assert_unequal(str, "b:q:p");
+    mu_assert_equal(str, "");
+
+    return 0;
+}
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 void SerialControllerTests() {
     printf("running tests for SerialCallback\n");
-    
+    mu_run_test("create and call void callback", scb_void);
+    mu_run_test("create and call string callback", scb_string);
+    mu_run_test("create and call integer callback", scb_int);
+    mu_run_test("create and call float callback", scb_float);
+
     printf("running tests for SerialController\n");
+    mu_run_test("normal operation", sctrl_normal);
+    mu_run_test("callback and key/value overflow", sctrl_overflow);
+
+    mu_run_test("ignore messages with no closing bracket", sctrl_malformed_closingbracket);
+    mu_run_test("ignore messages with no opening bracket", sctrl_malformed_openingbracket);
+    mu_run_test("ignore messages with no colon", sctrl_malformed_colon);
+    mu_run_test("process messages with multiple closing brackets", sctrl_malformed_multi_closingbracket);
+    mu_run_test("process messages with multiple opening brackets", sctrl_malformed_multi_openingbracket);
+    mu_run_test("ignore messages with multiple colons", sctrl_malformed_multi_colon);
 }
