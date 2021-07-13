e7b75bd253088caa163b6d1ff9de2c69fad993e9
sanine-a <sanine.not@pm.me>
2021-07-13 16:19:43 -0500

    implement fake EEPROM

--- Libraries/tests/FakeEEPROM.h
+++ Libraries/tests/FakeEEPROM.h
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <string.h>
+
 typedef unsigned char uint8_t;
 
 template<size_t numBytes>
 class _EEPROM {
 public:
-    _EEPROM() {}
-    uint8_t read(unsigned int index) { return 0; }
-    void write(unsigned int index, uint8_t value) {}
-    void update(unsigned int index, uint8_t value) {}
-    uint8_t& operator[](unsigned int index) { return memory[0]; }
-    void clear() {}
+    _EEPROM() { clear(); }
+    uint8_t read(unsigned int index) { return memory[index]; }
+    void write(unsigned int index, uint8_t value) { memory[index] = value; }
+    void update(unsigned int index, uint8_t value) { memory[index] = value; }
+    uint8_t& operator[](unsigned int index) { return memory[index]; }
+    void clear() { memset(memory, 255, numBytes); }
 private:
     uint8_t memory[numBytes];
 };
