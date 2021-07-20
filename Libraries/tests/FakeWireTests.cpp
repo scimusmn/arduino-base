2f090cd573b20464651d6800189cf7002aed3890
sanine-a <sanine.not@pm.me>
2021-07-20 11:23:01 -0500

    write basic rx/tx tests

--- Libraries/tests/FakeWireTests.cpp
+++ Libraries/tests/FakeWireTests.cpp
@@ -1,15 +1,46 @@
 #include "tests.h"
 #include "FakeWire.h"
 
+#include <string>
+
 
 class BasicPeripheral : public PeripheralDevice {
+public:
     void onReceive(int num_bytes) {}
     void onRequest() {}
+    std::string str;
 };
 
 
 mu_test fwire_basic_tx() {
     Wire.reset();
+    BasicPeripheral peripheral;
+    Wire.attach(peripheral, 0x70);
+
+    Wire.beginTransmission(0x70);
+    Wire.write('a');
+    Wire.write('b');
+    Wire.write('c');
+    Wire.endTransmission();
+
+    mu_assert_equal(peripheral.str, "abc");
+    
+    return 0;
+}
+
+
+mu_test fwire_basic_rx() {
+    Wire.reset();
+    BasicPeripheral peripheral;
+    Wire.attach(peripheral, 0x70);
+
+    Wire.requestFrom(0x70, 3);
+    mu_assert_equal(Wire.available(), 3);
+    mu_assert_equal(Wire.read(), 'a');
+    mu_assert_equal(Wire.read(), 'b');
+    mu_assert_equal(Wire.read(), 'c');
+
+    mu_assert_equal(Wire.available(), 0);
 
     return 0;
 }
@@ -23,6 +54,7 @@ void FakeWireTests() {
     printf("running tests for fake Wire\n");
 
     mu_run_test("transmit to peripheral", fwire_basic_tx);
+    mu_run_test("receive from peripheral", fwire_basic_rx);
 
     printf("  ran %d tests\n", tests_run - tests_run_old);
 }
