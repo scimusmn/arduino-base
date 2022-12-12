a88d494b47df2f930f1c075f0ec2fc9c55da7a33
sanine-a <sanine.not@pm.me>
2022-12-12 14:19:05 -0600

    add string::push_back

--- Libraries/string.hpp
+++ Libraries/string.hpp
@@ -13,14 +13,17 @@ class string {
 	char m_str[sz];
 
 	public:
-	string() {}
+	string() {
+		memset(m_str, 0, sz);
+	}
 
 	string(const char *s) {
+		memset(m_str, 0, sz);
 		operator=(s);
 	}
 
 	void operator=(const char* s) {
-		size_t len = strlen(s) + 1;
+		size_t len = strlen(s)+1;
 		if (len > sz) {
 			// not enough space to store this string
 			throw smm::out_of_memory("not enough space to copy string");
@@ -30,6 +33,22 @@ class string {
 
 	const char *c_str() {
 		return reinterpret_cast<const char*>(m_str);
+	}
+
+	void push_back(char ch) {
+		if (size() + 1 > sz) {
+			// not enough space to store an additional character
+			throw smm::out_of_memory("not enough space to push character");
+		}
+		strncat(m_str, &ch, 1);
+	}
+
+	size_t size() {
+		return strlen(m_str) + 1;
+	}
+
+	size_t max_size() {
+		return sz;
 	}
 };
 
--- Libraries/test/string.cpp
+++ Libraries/test/string.cpp
@@ -6,15 +6,32 @@ TEST_CASE("basic operator= and c_str()") {
 	smm::string<32> s;
 	s = "hello, world!";
 	CHECK(strcmp(s.c_str(), "hello, world!") == 0);
+	CHECK(s.size() == strlen("hello, world!")+1);
+	CHECK(s.max_size() == 32);
 }
 
 
 TEST_CASE("nice constructor=") {
-	smm::string<32> s = "hello, world!";
+	smm::string<32> s = "hello, world";
+	CHECK(strcmp(s.c_str(), "hello, world") == 0);
+	s.push_back('!');
 	CHECK(strcmp(s.c_str(), "hello, world!") == 0);
 }
 
 
 TEST_CASE("constructor overflow") {
 	CHECK_THROWS_AS(smm::string<2> s("hi"), smm::out_of_memory);
+}
+
+
+TEST_CASE("push more characters") {
+	smm::string<4> s;
+	CHECK(strcmp(s.c_str(), "") == 0);
+	s.push_back('a');
+	CHECK(strcmp(s.c_str(), "a") == 0);
+	s.push_back('b');
+	CHECK(strcmp(s.c_str(), "ab") == 0);
+	s.push_back('c');
+	CHECK(strcmp(s.c_str(), "abc") == 0);
+	CHECK_THROWS_AS(s.push_back('d'), smm::out_of_memory);
 }
